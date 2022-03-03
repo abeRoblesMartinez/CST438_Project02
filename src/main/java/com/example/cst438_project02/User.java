b618fd1fe21e3acd822aede5684e22097392600e
abeRoblesMartinez <aroblesmartinez@csumb.edu>
2022-03-02 23:00:24 -0800

    Created User classes for MYSQL Database (see Dr.C's video)

---
+++ src/main/java/com/example/cst438_project02/User.java
@@ -0,0 +1,41 @@
+package com.example.cst438_project02;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer user_id;
+
+    private String username;
+    private String email;
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/cst438_project02/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.cst438_project02;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
