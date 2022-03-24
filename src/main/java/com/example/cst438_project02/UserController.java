84bf549584d20ec0c2c663a758a1359a25eeeb3b
More3428 <andmoreno@csumb.edu>
2022-03-24 15:16:37 -0700

    Added Html to the main pages and JavaScript for the items

--- src/main/java/com/example/cst438_project02/UserController.java
+++ src/main/java/com/example/cst438_project02/UserController.java
@@ -1,6 +1,5 @@
 
 package com.example.cst438_project02;
-import com.example.cst438_project02.User;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,7 +20,7 @@ public class UserController
     public ModelAndView save(@ModelAttribute User user)
     {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("user-data");
+        modelAndView.setViewName("userdata");
         modelAndView.addObject("user", user);
         return modelAndView;
     }
