06d84ea93f76fd2ae48ff78e4c5523842e0dfae9
abeRoblesMartinez <aroblesmartinez@csumb.edu>
2022-04-10 22:55:23 -0700

    Fixed All Items page issues

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,3 @@
-
 displayItems();
 getItems();
 
@@ -35,38 +34,4 @@ async function fetchData(url){
     let data = await response.json();
     console.log(data);
     return data;
-
-const api_url = "http://fast-dawn-34687.herokuapp.com/api/allItems";
-
-async function api(api_url) {
-    const response = await fetch(api_url);
-
-    var data = await response.json();
-    console.log(data);
-    if(response){
-        hideloader();
-    }
-    show(data);
-}
-
-api(api_url);
-
-function hideloader(){
-    document.getElementById('loading').style.display = 'none';
-}
-
-function show(data) {
-    let tab =
-        `<tr>
-         <th>Item</th>
-         <th>Price</th>
-         <th>Description</th>
-         </tr>`;
-
-         for(let r of data.list) {
-         tab += ` <tr>
-        <td>${r.name}</td>
-        
-        <td>${r.info}</td>
-        </tr>`;
 }
